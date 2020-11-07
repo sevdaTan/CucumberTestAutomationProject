3fdbd12a3e4ec8e7f5b955dbd10c1a585831ffa8
sevdaTan <sevdatanyildizi@gmail.com>
2020-11-07 16:41:03 -0500

    failedRunner has been added to vytrack project

--- src/test/java/com/vytrack/utils/Driver.java
+++ src/test/java/com/vytrack/utils/Driver.java
@@ -3,7 +3,13 @@ package com.vytrack.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Driver {
     private static WebDriver driver;
@@ -22,6 +28,18 @@ public class Driver {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     break;
+                case"remote-chrome":
+
+                     try {
+                         // ChromeOptions chromeOptions = new ChromeOptions();
+                    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                    desiredCapabilities.setBrowserName("chrome");
+                        URL gridURL = new URL("http://35.172.114.211:4444/wd/hub");
+                         driver = new RemoteWebDriver(gridURL,desiredCapabilities);
+                     } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+
                 default:
                     throw new RuntimeException("No such a browser yet!");
             }
